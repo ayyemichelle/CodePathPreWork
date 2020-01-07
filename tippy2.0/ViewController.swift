7b31209fa5020f1edfdbed440757b21c72e17e85
ayyemichelle <michelle.v.aleman@gmail.com>
2020-01-06 18:24:40 -0800

    pre-work status

--- tippy2.0/ViewController.swift
+++ tippy2.0/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // ** many of the controls are different and the tutorial posted on youtube is now outdated. I wasn't able to figure out how to tie the segment event of the value being changed to this function. Dragging it as described in the tutorial did not work. **
+        
+        // get the bill
+        let bill = Double(billField.text!) ?? 0
+        // calculate tip and total
+        let tipPercentages = [0.15,0.18, 0.2]
+        print(tipControl.selectedSegmentIndex)
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip;
+        // update tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    
+    
 }
 
